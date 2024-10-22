6a9814262f7613964c207e87d15ebc3dbed4b5fd
Muhammad Asif <m.asif.addi@gmail.com>
2024-10-22 17:05:46 +0500

    Initial commit

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -7,6 +7,86 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 24.0, right: 24),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image(
+                      height: 65,
+                      width: 50,
+                      image: AssetImage('assets/logo.png'),
+                    ),
+                    SizedBox(
+                      width: 15,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Maintenance',
+                          style: TextStyle(
+                              fontSize: 24, fontFamily: 'Rubik Medium'),
+                        ),
+                        Text(
+                          'Box',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontFamily: 'Rubik Medium',
+                            color: Color(0xffF97038),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                const Center(
+                  child: Text(
+                    'Welcome to my flutter state management project with bloc',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16, fontFamily: 'Rubik Regular'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 250,
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 50,
+                    width: 400,
+                    decoration: BoxDecoration(
+                        color: Color(0xffF97038),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Center(
+                      child: Text(
+                        'Log In with Email',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'Rubik Regular',
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
